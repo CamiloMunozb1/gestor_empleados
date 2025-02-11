4b18ecebf30ec0227c20deb32cab092e653e4d34
Camilo Muñoz Bautista <141885846+CamiloMunozb1@users.noreply.github.com>
2025-02-10 19:44:46 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,26 +1,33 @@
-from funcionalidad.empleados import ConexionDB
-ruta = "C:/Users/POWER/ges_empleados.db.db"
-conexion = ConexionDB(ruta)
-
-
 while True:
+    # Mostramos el menú de opciones al usuario
     print("""
             Bienvenido al sistema de registro de empleados
             1. Registrar empleado
             2. Mostrar empleados
-            3. Eliminar empleado.
+            3. Eliminar empleado
             4. Salir
         """)
     try:
-        usuario = int(input("Ingrese una opcion: "))
+        # Solicitamos al usuario que ingrese una opción del menú
+        usuario = int(input("Ingrese una opción: "))
+        
+        # Verificamos la opción ingresada y llamamos al método correspondiente
         if usuario == 1:
+            # Opción para registrar un nuevo empleado
             conexion.insertar_empleados()
         elif usuario == 2:
+            # Opción para mostrar la lista de empleados
             conexion.mostrar_empleados()
         elif usuario == 3:
+            # Opción para eliminar un empleado existente
             conexion.eliminar_empleados()
         elif usuario == 4:
-            print("Gracias por usar el sistema, hasta la proxima.")
-            break
+            # Opción para salir del programa
+            print("Gracias por usar el sistema, hasta la próxima.")
+            break  # Rompemos el bucle while para finalizar el programa
+        else:
+            # Si el usuario ingresa un número fuera de las opciones válidas
+            print("Por favor, ingrese un número entre 1 y 4.")
     except ValueError:
-        print("Ingrese un valor numerico correcto...")
+        # Capturamos la excepción si el usuario ingresa un valor que no es un número entero
+        print("Ingrese un valor numérico correcto...")
